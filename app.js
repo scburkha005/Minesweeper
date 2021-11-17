1197ba1ca6715a0e4dd16e31ef237ebf70c957c7
scburkha005 <scburkha005@gmail.com>
2021-11-16 17:37:31 -0800

    Completed Mine generation algo and dom manipulation to appear in playing field

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ function newGame () {
         mineCount: 40,
         generateMines: function() {
             let mineCollection = [];
-            while (mineCollection.length < 40) {
+            while (mineCollection.length < this.mineCount) {
                 let minePosition = [];
                 for (let j = 0; j < 2; j++) {
                     let randomPosition = Math.floor(Math.random() * 16);
                     minePosition.push(randomPosition);
                 }
-                if (mineCollection.indexOf(minePosition) === -1) {
+                if (this.checkRepeatMine(minePosition, mineCollection)) {
                     mineCollection.push(minePosition);
                 }
             }
             return mineCollection;
+        },
+        checkRepeatMine: function(specificMine, allMines) {
+            let isUnique = true;
+            for (let i = 0; i < allMines.length; i++) {
+                if (specificMine[0] === allMines[i][0] && specificMine[1] === allMines[i][1]) {
+                    isUnique = false;
+                }
+            }
+            return isUnique;
         }
     }
     return game;
@@ -21,11 +30,18 @@ function newGame () {
 
 const xPosition = document.getElementsByClassName('column');
 console.log(xPosition);
-// const yPosition = xPosition[0].children;
-let test = document.getElementById('column1');
-console.log(test);
-let yTest = test.children;
 
+//initialize game
 let game = newGame();
 let mineArray = game.generateMines();
 console.log(mineArray);
+
+//add mines to the playfield
+for (i = 0; i < mineArray.length; i++) {
+    let minePosition = mineArray[i];
+    let minePlacement = xPosition[minePosition[0]].children[minePosition[1]];
+    // console.log(columnArray);
+    minePlacement.style.backgroundColor = 'red';
+}
+
+//test section
